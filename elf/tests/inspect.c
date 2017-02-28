aece07d7bce75e63d3bcd7c721842afa1a916b5b
Andre LaFleur <andre.lafleur@utah.edu>
2017-02-27 20:21:34 -0700

    Print section names

--- elf/tests/inspect.c
+++ elf/tests/inspect.c
@@ -12,9 +12,45 @@
    header pointer as `shdr`, returns a pointer to the memory that
    contains the in-memory content of the section */
 #define AT_SEC(ehdr, shdr) ((void *)(ehdr) + (shdr)->sh_offset)
+#define FUNC_ARRAY_SIZE 100
 
+/* These were defined for me */
 static void check_for_shared_object(Elf64_Ehdr *ehdr);
 static void fail(char *reason, int err_code);
+
+/* Definition of a function */
+typedef struct function {
+  char *func_name;
+  char *var_names;
+  struct function *next;
+}function;
+
+/* My functions */
+static void print_functions();
+static void get_all_functions(Elf64_Ehdr*);
+static void add_function(function f);
+
+// The current function
+static function *head;
+static function *current_function;
+
+/**
+ * GAMEPLAN
+ *
+ * So I'm pretty far over my head on this one, but I'm going to give it a shot.
+ *
+ * We can move through the file as many times as we want, so why don't we make
+ * a shitty datastructure and populate it as we go? Just grab the names of all
+ * the functions and then worry about throwing in the variables later.
+ *
+ * Here's what I'm thinking we could do:
+ * 
+ * - Find the .dynsym section. That will help us get a hold of the .dynstr
+ *   section, which will hold the function names
+ * - Loop through the functions until we get one that is not empty
+ * - Dump in the structure
+ * - Print out all the functions
+ */
 
 int main(int argc, char **argv) {
   int fd;
@@ -46,9 +82,39 @@ int main(int argc, char **argv) {
   check_for_shared_object(ehdr);
 
   /* Add a call to your work here */
+  get_all_functions(ehdr);
+  print_functions();
 
   return 0;
 }
+
+static void print_functions(){
+  // TODO
+}
+
+
+static void get_all_functions(Elf64_Ehdr *ehdr){
+  Elf64_Shdr* headers = (void*)ehdr + ehdr->e_shoff;
+  unsigned long strtab_offset = headers[ehdr->e_shstrndx].sh_offset;
+  char *strings = (void*)ehdr + strtab_offset;
+  int i;
+  for(i = 0; i < ehdr->e_shnum; i++){
+    printf("%s\n", strings + headers[i].sh_name);
+  }
+}
+
+/**
+ * Adds the function provided to the linked list.
+ */
+static void add_function(function f){
+  // TODO: This
+  if(head == NULL){
+    head = &f;
+    current_function = &f;
+  }
+}
+
+//////////////// HELPERS ///////////////////////
 
 static void check_for_shared_object(Elf64_Ehdr *ehdr) {
   if ((ehdr->e_ident[EI_MAG0] != ELFMAG0)
