f0f3341eba1e0da511778901cff4f8a5abd21ce4
Andre LaFleur <andre.lafleur@utah.edu>
2017-04-07 11:02:22 -0600

    Implement new_page()

--- malloc/mm.c
+++ malloc/mm.c
@@ -19,24 +19,53 @@
 
 /* always use 16-byte alignment */
 #define ALIGNMENT 16
-
-/* rounds up to the nearest multiple of ALIGNMENT */
 #define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~(ALIGNMENT-1))
-
-/* rounds up to the nearest multiple of mem_pagesize() */
 #define PAGE_ALIGN(size) (((size) + (mem_pagesize()-1)) & ~(mem_pagesize()-1))
 
-void *current_avail = NULL;
-int current_avail_size = 0;
+#define DEFAULT_PAGE_SIZE PAGE_ALIGN(2 * mem_pagesize())
+
+typedef struct page_header {
+  struct page_header *next;
+} page_header;
+
+typedef size_t block_header;
+
+// Some defs to help us navigate and manage things
+#define BLOCK_SIZE(blk)(*blk & 0xfffffffffffffff8)
+#define ALLOCATED(blk)(*blk & 0x01)
+#define TERMINATOR(blk)(BLOCK_SIZE(blk) == 0 && ALLOCATED(blk))
+
+#define FIRST_BLOCK(page) ((block_header*)&page[1])
+#define NEXT_BLOCK(blk)((block_header*)&blk[(BLOCK_SIZE(blk) + 8) / sizeof(block_header)]) // +8 for header
+#define PAYLOAD_OF(blk)((void*) &blk[1])
+
+// Functions to make our lives easier
+static page_header* new_page(size_t, page_header*);
+static void set_block(block_header*, size_t, int);
+
+// For debugging
+static void mem_check();
+
+static char buffer[32];
+static void wr();
+
+static int page_count = 0;
+static int block_count = 0;
+
+// The start of our list
+static page_header *first_page;
 
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
-  current_avail = NULL;
-  current_avail_size = 0;
+  first_page = new_page(DEFAULT_PAGE_SIZE, NULL);
   
+  if(first_page == NULL)
+    return -1;
+  
+  mem_check();
   return 0;
 }
 
@@ -46,21 +75,9 @@ int mm_init(void)
  */
 void *mm_malloc(size_t size)
 {
-  int newsize = ALIGN(size);
-  void *p;
-  
-  if (current_avail_size < newsize) {
-    current_avail_size = PAGE_ALIGN(newsize);
-    current_avail = mem_map(current_avail_size);
-    if (current_avail == NULL)
-      return NULL;
-  }
-
-  p = current_avail;
-  current_avail += newsize;
-  current_avail_size -= newsize;
-  
-  return p;
+  // TODO
+  mem_check();
+  return NULL;
 }
 
 /*
@@ -68,4 +85,90 @@ void *mm_malloc(size_t size)
  */
 void mm_free(void *ptr)
 {
+  // TODO
+  mem_check();
+}
+
+/* Returns the first block header of the new page */
+static page_header* new_page(size_t size, page_header *old) {
+  size = PAGE_ALIGN(size);
+  void *ptr = mem_map(size);
+
+  if (ptr == NULL)
+    return NULL;
+
+  page_header *next = &((page_header*)ptr)[0];
+
+  if (old != NULL)
+    old->next = next;
+
+  next->next = NULL;
+
+  block_header *first = FIRST_BLOCK(next);
+  set_block(first, size - 16, 0); // -16 for page_header and terminating block
+  block_header *term = NEXT_BLOCK(first);
+
+  set_block(term, 0, 1);
+
+  if(!(TERMINATOR(NEXT_BLOCK(first)))){
+    sprintf(buffer, "new_page does not size properly!\n");
+    wr();
+    exit(1);
+  }
+
+  return next;
+}
+
+static void set_block(block_header *hdr, size_t size, int alloc) {
+  *hdr = size;
+  *hdr += alloc;
+}
+
+
+/*
+ * Debugging functions
+ */
+
+static void wr(){
+  write(1, buffer, strlen(buffer));
+}
+
+static void mem_check(){
+  page_header *pg = first_page;
+  int pages = 0;
+
+  while(pg != NULL) {
+    if(pages > page_count){
+      sprintf(buffer, "Page count error!\n");
+      wr();
+      exit(1);
+    }
+
+    block_header *blk = FIRST_BLOCK(pg);
+    int blocks = 0;
+
+    while(!(TERMINATOR(blk))){
+
+      // Ensure we don't pass the terminator
+      if(blocks > block_count){
+	sprintf(buffer, "Block count error!\n");
+	wr();
+	exit(1);
+      }
+
+      // Ensure that we are aligned properly
+      if(((long)(blk)) % 16 != 8){
+	sprintf(buffer, "Block alignment error!\n");
+	wr();
+	exit(1);
+      }
+
+
+      block_count++;
+      blk = NEXT_BLOCK(blk);
+    }
+
+    pg = pg->next;
+    pages++;
+  }
 }
